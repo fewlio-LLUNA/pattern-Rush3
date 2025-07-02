02ae392d8224abb547b40dc9f36157de9cf5ab6c
fewlio <seraja0824ryoiki@gmail.com>
2025-07-02 13:57:40 +0900

    キー誘導表記追加

--- Assets/Scripts/KeyBeamController.cs
+++ Assets/Scripts/KeyBeamController.cs
@@ -7,6 +7,11 @@ public class KeyBeamController : MonoBehaviour
     public GameObject keyBeam3; // Jキー
     public GameObject keyBeam4; // Lキー
 
+    public GameObject keyBeamInfo1; // Aキー
+    public GameObject keyBeamInfo2; // Dキー
+    public GameObject keyBeamInfo3; // Jキー
+    public GameObject keyBeamInfo4; // Lキー
+
     void Start()
     {
         // 全て最初は非表示に
@@ -14,6 +19,11 @@ public class KeyBeamController : MonoBehaviour
         keyBeam2.SetActive(false);
         keyBeam3.SetActive(false);
         keyBeam4.SetActive(false);
+
+        keyBeamInfo1.SetActive(false);
+        keyBeamInfo2.SetActive(false);
+        keyBeamInfo3.SetActive(false);
+        keyBeamInfo4.SetActive(false);
     }
 
     void Update()
@@ -22,5 +32,10 @@ public class KeyBeamController : MonoBehaviour
         keyBeam2.SetActive(Input.GetKey(KeyCode.D));
         keyBeam3.SetActive(Input.GetKey(KeyCode.J));
         keyBeam4.SetActive(Input.GetKey(KeyCode.L));
+
+        keyBeamInfo1.SetActive(Input.GetKey(KeyCode.A));
+        keyBeamInfo2.SetActive(Input.GetKey(KeyCode.D));
+        keyBeamInfo3.SetActive(Input.GetKey(KeyCode.J));
+        keyBeamInfo4.SetActive(Input.GetKey(KeyCode.L));
     }
 }
